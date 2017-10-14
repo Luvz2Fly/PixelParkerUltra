842d2f80dbce213ffdce862ba830a9e2fc82f496
Gary R <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-13 21:20:27 -0400

    Meter Effect working now - solid color green

--- src/ultraTest.ino
+++ src/ultraTest.ino
@@ -14,57 +14,53 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB +
   NEO_KHZ800);
 
-int setDist = 7;
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
    Serial.begin(9600);
    strip.begin();
    strip.show(); // Initialize all pixels to 'off'
-   strip.setBrightness(8);
+   strip.setBrightness(50);
+   delay(50);
 }
+
 
 void loop() {
-   delay(50);
-   unsigned int median_time = sonar.ping_median(5);  // Get ping times averaged over (x) cycles
+
+   unsigned int median_time = sonar.ping_median(4);  // Get ping times averaged over (x) cycles
    unsigned int inch = sonar.convert_in(median_time); // convert times to inches
 
-   Serial.println(inch);  // DEBUG MODE - Show Inches
-
-   /*if (inch < 7 && inch != 0)   // SIMULATE DISTANCe change
-      {
-        Serial.println("ALERT");
-        Serial.println("=====");
-      }
-      */
-
-      distanceMeter(inch);    // pass current inches to meter function
-
-
+   clearRing(); // MUST Clear every loop to
+   distanceMeter(inch);    // pass current inches to meter function
 }
 
-//////// FUNCTIONS ////////////
+
+//////// FUNCTION LIBRARY ////////////
 
 void distanceMeter(int currentInches) {
+//Serial.println("distanceMeter:START");
 //Comments HERE = Me "Thinking out loud"
-// 16 Pixels used to mesure distance from 200cm (~80 Inches)
-// User Sets a STOP WARNING distance (apprx 10 inches from wall)
-// So we must map apprx 80"--->~10"   1px ---->16px
-// Fade from GREEN (7px) TO YELLOW->Orange (5px) TO Red (Last 4px)
+// 16 Pixels used to mesure distance from 200cm (~80 Inches) User Sets a STOP WARNING distance (apprx 10 inches from wall)
+// So we must map apprx 80"--->~10"   1px ---->16px  Fade from GREEN (7px) TO YELLOW->Orange (5px) TO Red (Last 4px)
 // For Testing purposes will set stopTarget = 10
-
 // curentInches = curent pinger value
 
-int stopTarget = 10;  //Testing Variable - Normally set by button
-int maxInches = 80;  // Going to ignore readings above 80 inches
-int mapPixel = map(currentInches,maxInches,stopTarget, 1, 15);
-// map the currentInches read from pinger using max range to StopTarget ranger
+int stopTarget = 4;  //Testing Variable - Normally set by button
+int maxInches = 30;  // Ignore readings above 80 inches
+int mapPixel = map(currentInches,maxInches,stopTarget, 0, 15);
+// map the currentInches from pinger using max range to StopTarget ranger
+// resulting value 0-15 represent the 16 pixels of the ring
 Serial.println(mapPixel);    //DEBUG PRINT MAPPED VALUE (What Pixel to light)
-strip.setPixelColor(mapPixel, 0, 255, 0);
-delay(300);
 clearRing();
+
+for(int i=0; i<mapPixel; i++) {
+  strip.setPixelColor(i, 0, 255, 0);
+  }
+
+strip.show();
+//delay(500);
 }
+
 
 void clearRing() {
   //Reset Ring
