2f19744af470ad86a060b5f009d05912a71f65a1
Gary R <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-15 12:31:44 -0400

    Small changes/formatting

--- src/PixelParkerUltra.ino
+++ src/PixelParkerUltra.ino
@@ -11,24 +11,49 @@ in the garage while pulling in... that can mean only one thing, it was time to b
 a digital ultrasonic NeoPixel parking assistant!
 
 Meet the "Pixel Parker Ultra 4000!"
-The code is ugly but it works and I will look for input on making this better...
+
+The code is ugly and dirty but it works - I would love input on making this better...
 
 Version 1.0 is simple...
 
-16 NeoPixel Pixel ring inerfaces with 
+Interface a cheap ultrasonic ranger with a NeoPixel ring to be used as a distance meter
+
+At bootup a defualt STOP target of 10 inches is set.  After that a momentary switch can
+be used to lock in a new target (park your car where you want it and press button, the
+new stop distace is set) MAGIC!
+
+Meter/LED's first starts displaying based on the range variable (maxInches) which
+can change to anything you want up to the pingers set maximum (#define MAX_DISTANCE)
+in cm. (to convert break out the ol' slide rule, calculator or just ask Alexa (shhh!))
+
+The 16 LED's on the ring 0 --> 15 are mapped from maxInches --> stopTarget to
+create the correct Pixel meter scale (1 pixel on = first detection at maxInches up to
+the last pixels (all on) coming on at stop target distance)
+
+The Pixel count is then used to determine the pixel colors. I picked arbitary
+(but logical points IMHO thank you) to swith ring from Green to Orange to Red to FLASHING
+
+Build some 3D cases and mount on the wall!!
+
+Option B - I thnk Amazon sells something like this for 10 bucks BUT thats not the point :)
+
+==================================
+
+WISH LIST (Mainly things taking advantage of NeoPixel's beauty!):
+ LED Start of  Green FADE around the ring from:
+  Green --> Yellow ---> Orange --> Red --> Flashing Red(ALL) -
+  Nice Fading effects
 
 
+Things to clean up:
+ Change hard coded Pixel count of 16 to a variable to be used with other sizes or shapes (that was selfish of me)
+ Fix mapping - I ended up with 17 pixels vs 16 (one of those silly '0' start thingys)
 
-WISH LIST:  LED Start Green FADE around the ring from:
- Green --> Yellow ---> Orange --> Red --> Flashing Red(ALL)
+I TAKE NO RESPONSABILITY FOR ANY DAMAGE TO YOUR VEHICLE, HOME, PETS OR POWER TOOLS
+WHILE USING THIS PARKING THING!  USE AT YOUR OWN RISK, SIDE EFECTS HAVE BEEN
+REPORTED, CALL YOUR DOCOR IF YOU CANT PARK YOUR CAR FOR MORE THAN 4 HOURS!
 
-
-
-Comments HERE = Me "Thinking out loud"
- 16 Pixels used to mesure distance from 200cm (~80 Inches) User Sets a STOP WARNING distance (apprx 10 inches from wall)
- So we must map apprx 80"--->~10"   1px ---->16px  Fade from GREEN (7px) TO YELLOW->Orange (5px) TO Red (Last 4px)
- For Testing purposes will set stopTarget = 10
-
+Have Fun!
 
 */
 
@@ -38,7 +63,7 @@ Comments HERE = Me "Thinking out loud"
 #define PIN 6
 #define TRIGGER_PIN 12
 #define ECHO_PIN 11
-#define MAX_DISTANCE 220
+#define MAX_DISTANCE 220   // Set PINGER limits in cm (After this it will return 0 as distance)
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB +
   NEO_KHZ800);
@@ -53,30 +78,30 @@ void setup() {
   Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(50);
-  delay(50);
+  strip.setBrightness(50); // This thing is BLINDING at full so I preset what the max brightness is here in percentage
+  delay(50); // Breathe..... it's go time!
 }
 
 
 void loop() {
 
-   unsigned int median_time = sonar.ping_median(10);  // Get ping times averaged over (x) cycles
-   unsigned int inch = sonar.convert_in(median_time); // convert times to inches
+   unsigned int median_time = sonar.ping_median(7);  // Get ping times averaged over (x) cycles
+   unsigned int inch = sonar.convert_in(median_time); // convert average times to inches
 
-   int buttonVal = digitalRead(2);   //PULL UP Resister = Closed HIGH
-   Serial.println(buttonVal);       // Looking for LOW to detect button push to ground
-   if (buttonVal == LOW) {          // Upon push set current INCH to TARGET
+   int buttonVal = digitalRead(2);   //internal PULL UP Resister => OFF = HIGH
+   Serial.println(buttonVal);       // Looking for LOW to detect button pushed to ground
+   if (buttonVal == LOW) {          // Upon push set current INCH to an ew STOP TARGET
        stopTarget = inch;
        Serial.println("Button Pressed");
        delay(250);
      }
 
-   clearRing(); // MUST Clear every loop to
-   distanceMeter(inch);    // pass current inches to meter function
+   clearRing(); // MUST Clear every loop to create meter effect
+   distanceMeter(inch);    // pass current inches to the meter function
 }
 
 
-//////// FUNCTION LIBRARY ////////////
+//////// START FUNCTIONS ////////////
 
 void distanceMeter(int currentInches) {
 // reminder to self:  curentInches = curent pinger value
@@ -112,7 +137,7 @@ else if (mapPixel == 16) {
 
 else if ((mapPixel <= 0) || (mapPixel >= 17)) {   // Out of range/Invalid range
   for(int i=0; i<mapPixel; i++) {
-    strip.setPixelColor(i, 0, 0, 255);
+    strip.setPixelColor(i, 0, 0, 128);
   }
 }
 
