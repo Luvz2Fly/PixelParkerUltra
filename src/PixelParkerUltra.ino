b0de18a1eec5fe9f63cc61e7e67624de1d143c0e
Gary R <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-15 11:38:02 -0400

    v1.0 Ready to Roll Added Flashing Red Alert at Target Distance Added Lots of notes at top

--- src/PixelParkerUltra.ino
+++ src/PixelParkerUltra.ino
@@ -1,6 +1,35 @@
 /*
- * Posted on http://randomnerdtutorials.com
- * created by http://playground.arduino.cc/Code/NewPing
+ * Thanks for the code borrowed from:
+ * http://randomnerdtutorials.com
+ * http://playground.arduino.cc/Code/NewPing
+
+Here is another installment of: "Solution in need of a Problem" Theater
+
+The other day I realized I had a couple of Ultra Sonic Pingers, a basket of Arduinos,
+4 hours of free time and a new dent on the hood of my car from hitting a shelf
+in the garage while pulling in... that can mean only one thing, it was time to build
+a digital ultrasonic NeoPixel parking assistant!
+
+Meet the "Pixel Parker Ultra 4000!"
+The code is ugly but it works and I will look for input on making this better...
+
+Version 1.0 is simple...
+
+16 NeoPixel Pixel ring inerfaces with 
+
+
+
+WISH LIST:  LED Start Green FADE around the ring from:
+ Green --> Yellow ---> Orange --> Red --> Flashing Red(ALL)
+
+
+
+Comments HERE = Me "Thinking out loud"
+ 16 Pixels used to mesure distance from 200cm (~80 Inches) User Sets a STOP WARNING distance (apprx 10 inches from wall)
+ So we must map apprx 80"--->~10"   1px ---->16px  Fade from GREEN (7px) TO YELLOW->Orange (5px) TO Red (Last 4px)
+ For Testing purposes will set stopTarget = 10
+
+
 */
 
 #include <NewPing.h>
@@ -15,7 +44,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB +
   NEO_KHZ800);
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-int stopTarget = 10;  //Testing Variable - Normally set by button
+int stopTarget = 10;  //Default alert inches after bootup
 
 
 void setup() {
@@ -30,14 +59,13 @@ void setup() {
 
 
 void loop() {
-   //delay(50);
 
    unsigned int median_time = sonar.ping_median(10);  // Get ping times averaged over (x) cycles
    unsigned int inch = sonar.convert_in(median_time); // convert times to inches
 
-   int buttonVal = digitalRead(2);
-   Serial.println(buttonVal);
-   if (buttonVal == LOW) {
+   int buttonVal = digitalRead(2);   //PULL UP Resister = Closed HIGH
+   Serial.println(buttonVal);       // Looking for LOW to detect button push to ground
+   if (buttonVal == LOW) {          // Upon push set current INCH to TARGET
        stopTarget = inch;
        Serial.println("Button Pressed");
        delay(250);
@@ -51,14 +79,8 @@ void loop() {
 //////// FUNCTION LIBRARY ////////////
 
 void distanceMeter(int currentInches) {
-//Serial.println("distanceMeter:START");
-//Comments HERE = Me "Thinking out loud"
-// 16 Pixels used to mesure distance from 200cm (~80 Inches) User Sets a STOP WARNING distance (apprx 10 inches from wall)
-// So we must map apprx 80"--->~10"   1px ---->16px  Fade from GREEN (7px) TO YELLOW->Orange (5px) TO Red (Last 4px)
-// For Testing purposes will set stopTarget = 10
-// curentInches = curent pinger value
-
-int maxInches = 60;  // Ignore readings above 80 inches
+// reminder to self:  curentInches = curent pinger value
+int maxInches = 80;  // Ignore readings above 80 inches
 int mapPixel = map(currentInches,maxInches,stopTarget, 0, 16);
 // map the currentInches from pinger using max range to StopTarget ranger
 // resulting value 0-15 represent the 16 pixels of the ring
@@ -66,7 +88,7 @@ Serial.print("Current Inches: "); Serial.println(currentInches);
 Serial.print("Current Stop Target: "); Serial.println(stopTarget);
 Serial.print("Turn on pixel: "); Serial.println(mapPixel);
 
-if ((mapPixel >= 0) && (mapPixel <= 10)) {  // 1 -> 10
+if ((mapPixel > 0) && (mapPixel <= 10)) {  // 1 -> 10
   for(int i=0; i<mapPixel; i++) {
     strip.setPixelColor(i, 0, 255, 0);
   }
@@ -78,18 +100,29 @@ else if ((mapPixel >10) && (mapPixel <= 14)) { // 11 -> 14
   }
 }
 
-else if (mapPixel > 14) {
+else if (mapPixel == 15) {
   for(int i=0; i<mapPixel; i++) {
     strip.setPixelColor(i, 255, 0, 0);
   }
 }
 
-//else {}
+else if (mapPixel == 16) {
+  redAlert();
+}
+
+else if ((mapPixel <= 0) || (mapPixel >= 17)) {   // Out of range/Invalid range
+  for(int i=0; i<mapPixel; i++) {
+    strip.setPixelColor(i, 0, 0, 255);
+  }
+}
 
 strip.show();
 //delay(500);
 }
 
+
+
+/////////////////////////////////////////////
 
 void clearRing() {
   //Reset Ring
@@ -98,3 +131,17 @@ void clearRing() {
   }
   strip.show();
   }
+
+
+////////////////////////////////////
+
+
+void redAlert() {
+    for(int x = 0; x <17; x++) {
+      strip.setPixelColor(x,255,0,0);
+  }
+
+  strip.show();
+  delay(250);
+  clearRing();
+}
